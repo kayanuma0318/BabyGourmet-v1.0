79e23616c07c1a7bf43f09fb642375cdb5c7a46c
N.kayanuma <kayanuma0318@gmail.com>
2024-08-18 10:29:18 +0900

    栄養素名、栄養量、バッチ取得メソッド簡略化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,30 @@ module ApplicationHelper
       # 予期せぬフラッシュメッセージの場合は、alert-infoを返す
     end
   end
+
+  # 各栄養素に対応する色を定義(recipes/show)
+  def nutrient_colors
+    {
+      'energy' => '#FCFF20',
+      'protein' => '#E3716C',
+      'dietary_fiber' => '#00b400',
+      'calcium' => '#00baff',
+      'magnesium' => '#8FBCBC',
+      'phosphorus' => '#691730',
+      'iron' => '#E6650D',
+      'zinc' => '#576275',
+      'iodine' => '#D1B6DD',
+      'vitamin_d' => '#ffcd00',
+      'vitamin_b6' => '#009c9c',
+      'vitamin_b12' => '#ff92aa',
+      'folate' => '#00843f',
+      'vitamin_c' => '#fadf9a',
+      'salt_equivalent' => '#00e9ff'
+    }
+  end
+
+  # 各栄養素名に対応する色をviewに返すメソッド(recipes/show)
+  def get_nutrient_color(nutrient)
+    nutrient_colors[nutrient]
+  end
 end
