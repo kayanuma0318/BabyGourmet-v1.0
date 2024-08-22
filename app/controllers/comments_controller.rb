348ac6be8f398ec7080107df9aa23b67043bc159
N.kayanuma <kayanuma0318@gmail.com>
2024-08-23 05:36:01 +0900

    非同期にてコメント作成機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,31 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to comment.recipe, success: t('messages.save_success', model: Comment.model_name.human)
-    else
-      flash.now[:danger] = t('messages.save_failed', model: Comment.model_name.human)
-      render 'recipes/show', status: :unprocessable_entity
+    @recipe = Recipe.find(params[:recipe_id])
+      # URLのパラメータからrecipe_idを取得し、IDの同じレシピを取得
+      # コメントを投稿するレシピを取得(コメントとレシピの関連付けが明確になる)
+    @comment = current_user.comments.build(comment_params)
+      # 新しいコメントを作成し、ログイン中のユーザーをコメントの投稿者として設定
+      # comment_paramsでフォームから送られてきたコメント情報を取得
+    @comment.recipe = @recipe
+      # 先ほど取得したレシピをコメントに紐付ける
+
+    respond_to do |format|
+      # リクエストのフォーマット(HTML,turbo_stream)によってレスポンスを分岐
+      if @comment.save
+        format.turbo_stream
+          # turbo_streamフォーマットのリクエストの場合
+          # create.turbo_stream.erbをレンダリング、新しいコメントを追加
+        format.html { redirect_to recipe_path(@comment.recipe), success: t('messages.save_success', model: Comment.model_name.human) }
+          # HTMLフォーマットのリクエストの場合
+          # コメントが追加されたレシピ(/recipe/:id)へリダイレクトする
+      else
+        format.turbo_stream
+          # turbo_streamフォーマットのリクエストの場合
+          # create.turbo_stream.erbをレンダリング、新しいコメントを追加
+        format.html { redirect_to recipe_path(@comment.recipe), danger: t('messages.save_failed', model: Comment.model_name.human) }
+          # HTMLフォーマットのリクエストの場合
+          # コメントを追加しようとしていたレシピページ(/recipe/:id)へリダイレクトする
+      end
     end
   end
 
@@ -15,15 +35,18 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    comment = Comment.find(params[:id])
-    comment.destroy
-    redirect_to comment.recipe, success: t('messages.delete_success', model: Comment.model_name.human)
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to recipe_path(@comment.recipe), success: t('messages.destroy_success', model: Comment.model_name.human) }
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body).merge(recipe_id: prams[:recipe_id])
+    params.require(:comment).permit(:body).merge(recipe_id: params[:recipe_id])
       # .merge(recipe_id: prams[:recipe_id]) :prams[:recipe_id]でURLのidを取得し、recipe_idとして使用
       # ( 送られてきたコメント情報がどのレシピに紐づいているのかを判定するために使用 )
       # .mergeを使用する理由: body(フォームデータ)とrecipe_id(URLパラメータ)を結合するため
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
 
-  validates :comment, presence: true
+  validates :body, presence: true
   # コメントの空欄禁止
 
-  validates :comment, length: { maximum: 256 }
+  validates :body, length: { maximum: 256 }
   # コメントの文字数制限256文字
 end
