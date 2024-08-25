c4cb08658ff9a2f6f461180bee7687e606d218f4
N.kayanuma <kayanuma0318@gmail.com>
2024-08-26 03:54:28 +0900

    [修正] コメント機能を非同期で作動するように修正
    変更の詳細：
    - comments_controller.rb(コントローラーを非同期対応できるよう修正)
    - routes.rb(commentsルーティングを設定)
    - comments/create.turbo_stream.erb(コメント作成時のturbo設定)
    - comments/_comment.html.erb(1つのコメント欄)
    - comments/_form.html.erb(コメント作成フォーム)
    - comments/edit.html.erb(コメント編集フォーム)
    - comments/update.turbo_stream.erb(コメント編集時のturbo設定)
    - comments/_comment_body.html.erb(コメント編集後のコメント欄)
    - comments/destroy.turbo_stream.erb(コメント削除時のturbo設定)
    
    関連するIssue: #147 コメント機能（非同期対応）
    
    追加: 新しい機能や要素の追加
    修正: バグの修正
    更新: 既存の機能の改善や変更
    削除: 機能や要素の削除
    文書: ドキュメントの変更
    その他: 上記に当てはまらない変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+    # ログインしていない場合、ログインページにリダイレクト
+  before_action :set_comment, only: %i[edit update destroy]
+    # ログイン中のユーザーのコメントを取得するメソッド
+
   def create
     @recipe = Recipe.find(params[:recipe_id])
       # URLのパラメータからrecipe_idを取得し、IDの同じレシピを取得
@@ -10,36 +15,37 @@ class CommentsController < ApplicationController
       # 先ほど取得したレシピをコメントに紐付ける
 
     respond_to do |format|
-      # リクエストのフォーマット(HTML,turbo_stream)によってレスポンスを分岐
       if @comment.save
         format.turbo_stream
           # turbo_streamフォーマットのリクエストの場合
-          # create.turbo_stream.erbをレンダリング、新しいコメントを追加
-        format.html { redirect_to recipe_path(@comment.recipe), success: t('messages.save_success', model: Comment.model_name.human) }
-          # HTMLフォーマットのリクエストの場合
-          # コメントが追加されたレシピ(/recipe/:id)へリダイレクトする
+          # create.turbo_stream.erbへリダイレクト
       else
         format.turbo_stream
           # turbo_streamフォーマットのリクエストの場合
-          # create.turbo_stream.erbをレンダリング、新しいコメントを追加
-        format.html { redirect_to recipe_path(@comment.recipe), danger: t('messages.save_failed', model: Comment.model_name.human) }
-          # HTMLフォーマットのリクエストの場合
-          # コメントを追加しようとしていたレシピページ(/recipe/:id)へリダイレクトする
+          # create.turbo_stream.erbへリダイレクト
       end
     end
   end
 
-  def edit
-    @comment = Comment.find(params[:id])
-      # @comment = viewで使用するため、インスタンス変数に代入
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @comment.update(edit_params)
+        format.turbo_stream
+        # update.turbo_stream.erbへリダイレクト
+      else
+        format.turbo_stream
+        # update.turbo_stream.erbへリダイレクト
+      end
+    end
   end
 
   def destroy
-    @comment = current_user.comments.find(params[:id])
     @comment.destroy!
     respond_to do |format|
       format.turbo_stream
-      format.html { redirect_to recipe_path(@comment.recipe), success: t('messages.destroy_success', model: Comment.model_name.human) }
+      # destroy.turbo_stream.erbへリダイレクト
     end
   end
 
@@ -51,5 +57,16 @@ class CommentsController < ApplicationController
       # ( 送られてきたコメント情報がどのレシピに紐づいているのかを判定するために使用 )
       # .mergeを使用する理由: body(フォームデータ)とrecipe_id(URLパラメータ)を結合するため
       # recipe_idを直接記述しない理由: 直接記述すると、フォームから送られてきたrecipe_idを許可してしまう可能性があるため
+  end
+
+  def edit_params
+    params.require(:comment).permit(:body)
+      # 編集では既にレシピIDが設定されているため、recipe_idを許可する必要がない
+      # 上記のように(comment_params)許可するとバリデーションエラーとなる（エラー現象:Recipeを入力してください。）
+  end
+
+  def set_comment
+    @comment = current_user.comments.find(params[:id])
+    # ログイン中のユーザーがコメントしたコメントを取得
   end
 end
