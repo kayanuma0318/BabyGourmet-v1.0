04c6c23c6caa9dc91550a109b74b72f6797e48c7
N.kayanuma <kayanuma0318@gmail.com>
2024-09-10 07:27:01 +0900

    タイトル
    [] 簡単な説明
    
    変更の詳細：
    - （）
    - （）
    - （）
    
    関連するIssue: #
    
    追加: 新しい機能や要素の追加
    修正: バグの修正
    更新: 既存の機能の改善や変更
    削除: 機能や要素の削除
    文書: ドキュメントの変更
    その他: 上記に当てはまらない変更

--- app/controllers/daily_menus_controller.rb
+++ app/controllers/daily_menus_controller.rb
@@ -13,19 +13,20 @@ class DailyMenusController < ApplicationController
   # 1. ログインしているユーザーが今日の献立にレシピを追加する
   # 2. findメソッドでレシピを取得
   # 3. 対象のレシピを引数にして、daily_menuメソッドを呼び出す
-  # 4. daily_menuメソッドがtrueを返した場合、create.turbo_stream.erbに送る
-  # 5. daily_menuメソッドがfalseを返した場合、daily_menus/_error.html.erbを表示する
+  # 4. レスポンスを create.turbo_stream.erb に返す
 
   def destroy
     daily_menu = current_user.daily_menus.find_by(id: params[:id])
     @recipe = daily_menu.recipe
     current_user.undaily_menu(@recipe)
-      # undaily_menu(recipe) = user.rbに記載
-      # ログインしているユーザーの今日の献立の中から、指定されたidのレコードを取得
-      # 削除対象のレシピを取得
 
     respond_to do |format|
       format.turbo_stream
     end
   end
 end
+  # 1. undaily_menu(recipe) = user.rbに記載
+  # 2. ログインしているユーザーの今日の献立の中から、指定されたidのレコードを取得
+  # 3. 削除対象のレシピを取得
+  # 4. 対象のレシピを引数にして、undaily_menuメソッドを呼び出す
+  # 5. レスポンスを destroy.turbo_stream.erb に返す
