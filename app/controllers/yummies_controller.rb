df50be06168af1d0265319d4cf4817846180ab64
N.kayanuma <kayanuma0318@gmail.com>
2024-09-11 05:22:04 +0900

    各ボタンの非同期処理修正
    [修正] 各ボタンが修正前ではページがリロードしていたので、非同期処理にて切り替わるように修正
    
    変更の詳細：
    - cook_laters_controller.rb（インスタンス変数を定義）
    - yummies_controller.rb（インスタンス変数を定義）
    - 各_button.html.erb（ローカル変数をインスタンス変数へ修正）
    - 各create.turbo_stream.erb (ローカル変数をインスタンス変数へ修正)
    - 各destory.turbo_stream.erb (ローカル変数をインスタンス変数へ修正)
    
    関連するIssue: #180 Yummy機能、作りたいものボタンの非同期処理修正
    
    追加: 新しい機能や要素の追加
    修正: バグの修正
    更新: 既存の機能の改善や変更
    削除: 機能や要素の削除
    文書: ドキュメントの変更
    その他: 上記に当てはまらない変更

--- app/controllers/yummies_controller.rb
+++ app/controllers/yummies_controller.rb
@@ -2,28 +2,24 @@ class YummiesController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
 
   def create
-    recipe = Recipe.find(params[:recipe_id])
-    yummy = current_user.yummies.build(recipe: recipe)
+    @recipe = Recipe.find(params[:recipe_id])
+    yummy = current_user.yummies.build(recipe: @recipe)
       # newではなく、buildメソッドを使用することで、user_idを自動で設定される
 
     respond_to do |format|
       if yummy.save
         format.turbo_stream
-        redirect_to request.referer
-          # リクエスト元にリダイレクトする
       end
     end
   end
 
   def destroy
-    recipe = Recipe.find(params[:recipe_id])
-    yummy = current_user.yummies.find_by(recipe_id: recipe.id)
+    @recipe = Recipe.find(params[:recipe_id])
+    yummy = current_user.yummies.find_by(recipe_id: @recipe.id)
 
     respond_to do |format|
       if yummy.destroy
         format.turbo_stream
-        redirect_to request.referer
-          # リクエスト元にリダイレクトする
       end
     end
   end
