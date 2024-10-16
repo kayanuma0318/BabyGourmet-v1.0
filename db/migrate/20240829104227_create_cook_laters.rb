dbced9182e887f19bba252c41cfc316921165a7f
N.kayanuma <kayanuma0318@gmail.com>
2024-10-17 08:41:32 +0900

    タイトル
    [] 簡単な説明
    
    変更の詳細：
    - ファイル名（修正内容）
    - （）
    - （）
    
    関連するIssue: #
    
    追加: 新しい機能や要素の追加
    修正: バグの修正
    更新: 既存の機能の改善や変更
    削除: 機能や要素の削除
    文書: ドキュメントの変更
    その他: 上記に当てはまらない変更

--- db/migrate/20240829104227_create_cook_laters.rb
+++ db/migrate/20240829104227_create_cook_laters.rb
@@ -5,8 +5,9 @@ class CreateCookLaters < ActiveRecord::Migration[7.1]
       t.references :recipe, foreign_key: true
 
       t.timestamps
-      add_index :cook_laters, [:user_id, :recipe_id], unique: true
-        # 同じuser_idとrecipe_idの組み合わせが重複しないようにする
+
     end
+    add_index :cook_laters, [:user_id, :recipe_id], unique: true
+      # user_idとrecipe_idの組み合わせが重複しないようにする
   end
 end
